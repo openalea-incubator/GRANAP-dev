b296ed49680c2c85b24d33d1dea3dcc8f42e35ff
Adrien Heymans <adrien.heymans@uclouvain.be>
2026-04-16 10:28:41 +0200

    Update test_root_features.py

--- test/test_root_features.py
+++ test/test_root_features.py
@@ -1,23 +1,47 @@
 
 import sys
 import os
+import time
 import matplotlib.pyplot as plt
 
 # Add parent directory to path to allow importing anatomy package
 sys.path.append(os.path.abspath('..'))
 
 from granap.root_class import RootAnatomy
-from granap.visualization import plot_layers_simple, plot_section
 
+t_start = time.time()
 # Create a needle anatomy
 root = RootAnatomy()
+root.update_params(param_name="aerenchyma", attribute="aerenchyma_proportion", value = 0.8)
+root.update_params(param_name="aerenchyma", attribute="n_files", value = 20)
+root.update_params(param_name="cortex", attribute="n_layers", value = 15)
+root.update_params(param_name="endodermis", attribute="n_layers", value = 10)
+print(root.params)
 
-root.update_params("inter_cellular_spaces", "tissue", "cortex")
-root.update_params("inter_cellular_spaces", "smoothness", 0.05)
-# root.plot_layers(show=True, title=f"Root Layers")
 
-root.plot_cells(show=True, title=f"Root Cells")
+root.generate_cells()
+t_end = time.time()
+print("Time to generate cells:", t_end - t_start)
 
+# Plot the needle and root anatomy in a 1x2 grid
+fig, axs = plt.subplots(1, 2, figsize=(20, 10), sharex=False, sharey=False)
 
+# Plot the root cells
+t_start = time.time()
+root.plot_cells(show=False, title=f"Root Cells", ax=axs[0])
+t_end = time.time()
+print("Time to plot cells:", t_end - t_start)
+
+# Export to adjacency matrix
+t_start = time.time()
 _ = root.export_to_adjencymatrix()
-root.plot_network(show=True, title="Root Network")
+t_end = time.time()
+print("Time to export to adjacency matrix:", t_end - t_start)
+
+# Plot the root network
+t_start = time.time()
+root.plot_network(show=False, title="Root Network", ax=axs[1])
+t_end = time.time()
+print("Time to plot network:", t_end - t_start)
+
+plt.show()
